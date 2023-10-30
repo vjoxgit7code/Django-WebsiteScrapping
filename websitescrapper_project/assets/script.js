8c38b5521b9865fba99fb5ed45c76aa472c55d59
vignesh <148747958+vjoxgit7code@users.noreply.github.com>
2023-10-30 15:53:54 +0530

    file upload

---
+++ websitescrapper_project/assets/script.js
@@ -0,0 +1,7 @@
+<script>
+    document.querySelector('.btn-warning').addEventListener('click', function () {
+        if (confirm("Are you sure you want to clear the form data?")) {
+            // Perform the clear action here
+        }
+    });
+</script>
---
+++ websitescrapper_project/websitescrapper_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WebsitescrapperAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'websitescrapper_app'
